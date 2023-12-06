5e451b8ebd91b063239b010353f44fa0a51d6c54
gerpag <germinalfp@gmail.com>
2023-12-06 13:57:22 +0100

    multer configuration and model modification,product  controller and service modifications

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -1,37 +1,133 @@
 const ProductServices=require("../services/products.services")
+const multer = require('multer');
+const path = require('path');
 
-class ProductControllers{
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, '../public/images'),
+    filename: (req, file, cb) => {
+      cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    },
+  });
+  
+  const upload = multer({ storage }).single('image');
+  
+  class ProductControllers {
+    static async addProduct(req, res) {
 
-    static async addProduct(req,res){
-        const {name,category,sub_category,url_img,colour}=req.body
-
-        try{
-            const newProduct=await ProductServices.addProduct({name,category,sub_category,url_img,colour})
-            res.status(201).json(newProduct)
+      upload(req, res, async (err) => {
+        if (err) {
+          return res.status(400).json({ error: 'Error uploading image.' });
         }
+        
+        console.log('Form Data:', req.body);
 
-        catch(error){
-            console.log(error)
-        res.status(400).json({ error: error.message });
-
+        const {
+          image,
+          name,
+          category,
+          subCategory,
+          material,
+          description,
+          color,
+          dimensions,
+          quantity,
+        } = req.body;
+  
+        const url_img = `/images/${req.file.filename}`;
+  
+        try {
+          const newProduct = await ProductServices.addProduct({
+            image,
+            name,
+            category,
+            subCategory,
+            material,
+            description,
+            color,
+            dimensions,
+            quantity,
+            url_img,
+          });
+          res.status(201).json(newProduct);
+        } catch (error) {
+          console.log(error);
+          res.status(400).json({ error: error.message });
         }
+      });
     }
-
-    static async getProducts(req,res){
-        const {category,sub_category,colour}=req.query
-
-        try{
-            const products=await ProductServices.getProducts({category,sub_category,colour})
-
-            res.status(200).json(products)
-
-        }
-        catch(error){
-            console.log(error)
+  
+    static async getProducts(req, res) {
+      const { category, subCategory, colour } = req.query;
+  
+      try {
+        const products = await ProductServices.getProducts({
+          category,
+          subCategory,
+          colour,
+        });
+  
+        res.status(200).json(products);
+      } catch (error) {
+        console.log(error);
         res.status(400).json({ error: error.message });
-
-        }
+      }
     }
-}
+  }
+  
+  module.exports = ProductControllers;
 
-module.exports=ProductControllers
+
+// const ProductServices=require("../services/products.services")
+// const multer = require('multer');
+// const path = require('path');
+
+
+// const storage = multer.diskStorage({
+//   destination: path.join(__dirname, '../public/images'), 
+//   filename: (req, file, cb) => {
+//     cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+//   },
+// });
+
+// const upload = multer({ storage }).single('image');
+
+// class ProductControllers {
+//   static async addProduct(req, res) {
+//     upload(req, res, async (err) => {
+//       if (err) {
+//         return res.status(400).json({ error: 'Error uploading image.' });
+//       }
+
+//       const { name, category, sub_category, colour } = req.body;
+//       const url_img = `/images/${req.file.filename}`; 
+
+//       try {
+//         const newProduct = await ProductServices.addProduct({ name, category, sub_category, url_img, colour });
+//         res.status(201).json(newProduct);
+//       } catch (error) {
+//         console.log(error);
+//         res.status(400).json({ error: error.message });
+//       }
+//     });
+//   }
+
+
+
+//     static async getProducts(req,res){
+//         const {category,sub_category,colour}=req.query
+
+//         try{
+//             const products=await ProductServices.getProducts({category,sub_category,colour})
+
+//             res.status(200).json(products)
+
+//         }
+//         catch(error){
+//             console.log(error)
+//         res.status(400).json({ error: error.message });
+
+//         }
+//     }
+// }
+
+// module.exports=ProductControllers
--- services/products.services.js
+++ services/products.services.js
@@ -1,68 +1,152 @@
-const {Product}=require("../models/index.model")
+
+const { Product } = require('../models/index.model');
 
 
-class ProductServices{
 
-    static async addProduct(data){
-        const {name,category,sub_category,url_img,colour}=data
+class ProductServices {
+  static async addProduct(data) {
+    const {
+      name,
+      image,
+      category,
+      subCategory,
+      material,
+      description,
+      color,
+      dimensions,
+      quantity,
+      url_img,
+    } = data;
 
-        try{
+    try {
+      if (!name || !category || !subCategory || !material || !description || !color || !dimensions || !quantity || !url_img) {
+        throw new Error('Debe completar todos los campos');
+      }
 
-            if(!name ||!category ||!sub_category ||!url_img || !colour){
-                throw new Error("Debe completar todos los campos")
-            }
+      const newProduct = await Product.create({
+        name,
+        image,
+        category,
+        sub_category: subCategory,
+        material,
+        description,
+        colour: color,
+        dimensions,
+        quantity,
+        url_img,
+      });
 
-            const newProduct= await Product.create({name,category,sub_category,url_img,colour})
+      return newProduct;
 
-            return newProduct;
-
-        }
-
-        catch (error) {
-            // Verifica si el error proviene de una respuesta del servidor
-            if (error.response && error.response.data) {
-              throw new Error(error.response.data);
-            } else {
-              // Si es otro tipo de error, lanza el error original
-              throw error;
-            }
-          }
-
-    }
-
-    static async getProducts(data){
-
-        const {category,sub_category,colour}=data
-
-        try{
-
-            const searchQuerys={}
-
-            if(category){
-                searchQuerys.category=category
-            }
-            if(sub_category){
-                searchQuerys.sub_category=sub_category
-            }
-            if(colour){
-                searchQuerys.colour=colour
-            }
-
-            const products= await Product.find(searchQuerys)
-
-                return products
-        
-            }
-          
-        catch (error) {
       
-            if (error.response && error.response.data) {
-              throw new Error(error.response.data);
-            } else {
-              throw error;
-            }
-          }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        throw new Error(error.response.data);
+      } else {
+        throw error;
+      }
     }
+  }
+
+  static async getProducts(data) {
+    const { category, subCategory, colour } = data;
+
+    try {
+      const searchQuerys = {};
+
+      if (category) {
+        searchQuerys.category = category;
+      }
+      if (subCategory) {
+        searchQuerys.sub_category = subCategory;
+      }
+      if (colour) {
+        searchQuerys.colour = colour;
+      }
+
+      const products = await Product.find(searchQuerys);
+
+      return products;
+    } catch (error) {
+      if (error.response && error.response.data) {
+        throw new Error(error.response.data);
+      } else {
+        throw error;
+      }
+    }
+  }
 }
 
-module.exports=ProductServices;
+module.exports = ProductServices;
+
+
+
+
+// const {Product}=require("../models/index.model")
+
+
+// class ProductServices{
+
+//     static async addProduct(data){
+//         const {name,category,sub_category,url_img,colour}=data
+
+//         try{
+
+//             if(!name ||!category ||!sub_category ||!url_img || !colour){
+//                 throw new Error("Debe completar todos los campos")
+//             }
+
+//             const newProduct= await Product.create({name,category,sub_category,url_img,colour})
+
+//             return newProduct;
+
+//         }
+
+//         catch (error) {
+//             // Verifica si el error proviene de una respuesta del servidor
+//             if (error.response && error.response.data) {
+//               throw new Error(error.response.data);
+//             } else {
+//               // Si es otro tipo de error, lanza el error original
+//               throw error;
+//             }
+//           }
+
+//     }
+
+//     static async getProducts(data){
+
+//         const {category,sub_category,colour}=data
+
+//         try{
+
+//             const searchQuerys={}
+
+//             if(category){
+//                 searchQuerys.category=category
+//             }
+//             if(sub_category){
+//                 searchQuerys.sub_category=sub_category
+//             }
+//             if(colour){
+//                 searchQuerys.colour=colour
+//             }
+
+//             const products= await Product.find(searchQuerys)
+
+//                 return products
+        
+//             }
+          
+//         catch (error) {
+      
+//             if (error.response && error.response.data) {
+//               throw new Error(error.response.data);
+//             } else {
+//               throw error;
+//             }
+//           }
+//     }
+// }
+
+// module.exports=ProductServices;
