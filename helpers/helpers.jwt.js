746f69e312a2715be23ae5e27639d60605b07ecd
alexis <as.alexismartinez@gmail.com>
2023-12-07 10:18:32 -0300

    feat: add routes persistence and protected routes

---
+++ helpers/helpers.jwt.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const generateJWT = (id, is_admin) => {
+  return jwt.sign({ id, is_admin }, process.env.JWT_SECRET, {
+    expiresIn: "20d",
+  });
+};
+const verifyJWT = (token) => {
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    return { payload };
+  } catch (error) {
+    return { error };
+  }
+};
+
+module.exports = { generateJWT, verifyJWT };
