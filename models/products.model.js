b1ca4eda4490fa688f73dd683999ea32ca18d5b3
gerpag <germinalfp@gmail.com>
2023-12-09 15:07:40 +0100

    change change unique field to false in product model

--- models/products.model.js
+++ models/products.model.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      unique: true,
+      unique: false,
     },
     category: {
       type: String,
